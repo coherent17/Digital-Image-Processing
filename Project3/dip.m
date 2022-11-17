3c82937e43b93b955fc519ebafaa13b7cd88e30a
coherent <mnb51817@gmail.com>
2022-11-17 13:08:35 +0800

    finish lab3

--- Project3/dip.m
+++ Project3/dip.m
@@ -31,8 +31,8 @@ denoise_image(1:M,1:N) = image_pad(3:802,3:802);
 %Generate Gaussian LPF & Butterworth LPF:
 
 beta = 1;
-n = 6;
-ButterWorth_D0 = 180;
+n = 5;
+ButterWorth_D0 = 200;
 
 GLPF_100 = zeros(2*M,2*N);
 GLPF_150 = zeros(2*M,2*N);
@@ -44,20 +44,20 @@ BLPF = zeros(M,N);
 for u = 1:2*M
     for v = 1:2*N
         D2 = (u-M)^2 + (v-N)^2;
-        GLPF_100(u,v) = exp(-1*D2/(2*100*100)); 
-        GLPF_150(u,v) = exp(-1*D2/(2*150*150)); 
-        GLPF_200(u,v) = exp(-1*D2/(2*200*200)); 
-        GLPF_250(u,v) = exp(-1*D2/(2*250*250)); 
+        GLPF_100(u,v) = exp(-1*D2/(2*100*100));
+        GLPF_150(u,v) = exp(-1*D2/(2*150*150));
+        GLPF_200(u,v) = exp(-1*D2/(2*200*200));
+        GLPF_250(u,v) = exp(-1*D2/(2*250*250));
         BLPF(u,v) = 1/(1+beta*(D2/(ButterWorth_D0*ButterWorth_D0))^n);
     end
 end
 
 %image pass inverse filter
-threshold = 1e-10;    %deal with divide by zero
-devonvolution_GLPF_100_ = real(ifft2(ifftshift(fftshift(fft2(denoise_image, 2*M, 2*N)).*BLPF./(GLPF_100+threshold))));
-devonvolution_GLPF_150_ = real(ifft2(ifftshift(fftshift(fft2(denoise_image, 2*M, 2*N)).*BLPF./(GLPF_150+threshold))));
-devonvolution_GLPF_200_ = real(ifft2(ifftshift(fftshift(fft2(denoise_image, 2*M, 2*N)).*BLPF./(GLPF_200+threshold))));
-devonvolution_GLPF_250_ = real(ifft2(ifftshift(fftshift(fft2(denoise_image, 2*M, 2*N)).*BLPF./(GLPF_250+threshold))));
+eps = 1e-6;
+devonvolution_GLPF_100_ = real(ifft2(ifftshift(fftshift(fft2(denoise_image, 2*M, 2*N)).*BLPF./(GLPF_100+eps))));
+devonvolution_GLPF_150_ = real(ifft2(ifftshift(fftshift(fft2(denoise_image, 2*M, 2*N)).*BLPF./(GLPF_150+eps))));
+devonvolution_GLPF_200_ = real(ifft2(ifftshift(fftshift(fft2(denoise_image, 2*M, 2*N)).*BLPF./(GLPF_200+eps))));
+devonvolution_GLPF_250_ = real(ifft2(ifftshift(fftshift(fft2(denoise_image, 2*M, 2*N)).*BLPF./(GLPF_250+eps))));
 
 %crop the image
 devonvolution_GLPF_100 = devonvolution_GLPF_100_(1:M, 1:N);
@@ -81,14 +81,21 @@ img2 = getframe(gcf);
 imwrite(img2.cdata, 'result/after_alpha_trim_filter_result.tiff', 'tiff', 'Resolution', 200);
 
 figure(3)
-subplot(2,2,1)
 imshow(uint8(devonvolution_GLPF_100));
+img3 = getframe(gcf);
+imwrite(img3.cdata, 'result/devonvolution_GLPF_100.tiff', 'tiff', 'Resolution', 200);
 
-subplot(2,2,2)
+figure(4)
 imshow(uint8(devonvolution_GLPF_150));
+img4 = getframe(gcf);
+imwrite(img4.cdata, 'result/devonvolution_GLPF_150.tiff', 'tiff', 'Resolution', 200);
 
-subplot(2,2,3)
+figure(5)
 imshow(uint8(devonvolution_GLPF_200));
+img5 = getframe(gcf);
+imwrite(img5.cdata, 'result/devonvolution_GLPF_200.tiff', 'tiff', 'Resolution', 200);
 
-subplot(2,2,4)
+figure(6)
 imshow(uint8(devonvolution_GLPF_250));
+img6 = getframe(gcf);
+imwrite(img6.cdata, 'result/devonvolution_GLPF_250.tiff', 'tiff', 'Resolution', 200);
